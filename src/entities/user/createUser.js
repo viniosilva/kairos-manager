3dc5838febc23b84a27f6d60217e5a42c11bd102
Vinicius de Oliveira Silva <oliveiravinicius92@gmail.com>
2020-03-30 14:35:02 -0300

    add createUser function

---
+++ src/entities/user/createUser.js
@@ -0,0 +1,21 @@
+const sequelize = require('../../database/sequelize');
+const { ValidationError } = require('../../common/errors');
+const User = require('./User');
+
+const { ValidationError: ValidationErrorSequelize } = sequelize.Sequelize;
+
+module.exports = async (payload) => {
+  try {
+    return await User.create(payload);
+  } catch (error) {
+    if (error instanceof ValidationErrorSequelize) {
+      const messageError = error.errors[0]
+        ? error.errors[0].message
+        : error.message;
+
+      throw new ValidationError(messageError);
+    }
+
+    throw error;
+  }
+};
