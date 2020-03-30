67429069406cefa026b2a14ac9e7318267cf6b1b
Vinicius de Oliveira Silva <oliveiravinicius92@gmail.com>
2020-03-30 16:59:56 -0300

    add error handler

---
+++ src/api/helpers/errorHandler.js
@@ -0,0 +1,18 @@
+const boom = require('@hapi/boom');
+const logger = require('./logger');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (error, _req, res, next) => {
+  let boomError;
+
+  if (boom.isBoom(error)) {
+    boomError = error;
+  } else {
+    logger.error({ error });
+    boomError = boom.internal();
+  }
+
+  const { payload, statusCode } = boomError.output;
+
+  res.status(statusCode).json(payload);
+};
